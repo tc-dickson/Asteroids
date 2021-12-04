083703d39cc8960900b358ae39c089f1201069f1
Tyler Dickson <tylercdickson@gmail.com>
2021-12-04 14:24:50 -0700

    Edit linearAlg.c and linearAlg.h
    Add the function prototype for the dotProduct function in linearAlg.c
    Change the type of the x, y coordinates of vector2D from uint16_t to int16_t to allow for negative numbers.

--- linearAlg.c
+++ linearAlg.c
@@ -1,19 +1,22 @@
 #include "linearAlg.h"
 #include <stdint.h>
 
+// Function Declarations.
+uint16_t dotProduct(vector2D_t vect1, vector2D_t vect2);
+
 // A function that calculates the given vector left multiplied by the given
 // matrix. This will be used to calculate rotations.
-vector2D matVectMultAx2D(matrix2x2 matrix, vector2D vector) {
+vector2D_t matVectMultAx2D(matrix2x2_t matrix, vector2D_t vector) {
   // Calculate the vector left multiplied by the matrix.
-  vector2D returnVect;
-  returnVect.x =
-      dotProduct((vector2D){.x = matrix.vect1.x, .y = matrix.vect2.x}, vector);
-  returnVect.y =
-      dotProduct((vector2D){.x = matrix.vect1.y, .y = matrix.vect2.y}, vector);
+  vector2D_t returnVect;
+  returnVect.x = dotProduct(
+      (vector2D_t){.x = matrix.vect1.x, .y = matrix.vect2.x}, vector);
+  returnVect.y = dotProduct(
+      (vector2D_t){.x = matrix.vect1.y, .y = matrix.vect2.y}, vector);
   return returnVect;
 }
 
 // Calculate the dot product of two vectors vect1 and vect2.
-uint16_t dotProduct(vector2D vect1, vector2D vect2) {
+uint16_t dotProduct(vector2D_t vect1, vector2D_t vect2) {
   return (vect1.x * vect2.x) + (vect1.y * vect2.y);
 }
--- linearAlg.h
+++ linearAlg.h
@@ -5,19 +5,19 @@
 
 // Declaration of a struct to hold a 2D vector.
 typedef struct {
-  uint16_t x; // Use a 16 bit number to hold all the possible values for the x-y
-              // coordinates of the LCD screen.
-  uint16_t y;
-} vector2D;
+  int16_t x; // Use a 16 bit number to hold all the possible values for the x-y
+             // coordinates of the LCD screen.
+  int16_t y;
+} vector2D_t;
 
 // Declaration of a struct to hold a 2x2 matrix.
 typedef struct {
-  vector2D vect1;
-  vector2D vect2;
-} matrix2x2;
+  vector2D_t vect1;
+  vector2D_t vect2;
+} matrix2x2_t;
 
 // A function that calculates the given vector left multiplied by the given
 // matrix. This will be used to calculate rotations.
-vector2D matVectMultAx2D(matrix2x2, vector2D);
+vector2D_t matVectMultAx2D(matrix2x2_t, vector2D_t);
 
 #endif // LINEARALG_H_
